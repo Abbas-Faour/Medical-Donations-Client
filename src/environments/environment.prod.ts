d8da27c0d21dbc65cc2832a53c45e7bca61c9d01
Abbas-Faour <abbasfaour25@gmail.com>
2022-07-08 12:36:20 +0300

    Change Azure URLs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
 };
 
-export const backend : string = "https://medical-donations-server.azurewebsites.net/api"
+export const backend : string = "https://medical-donations-server1.azurewebsites.net/api"
 export const blobStorage : string = "https://faour.blob.core.windows.net/images/"
